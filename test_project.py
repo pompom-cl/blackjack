1c8dbdcd1710f3e74b9fdda87d1b534767444740
Clara Lie <dazelwhale@gmail.com>
2024-05-30 14:55:44 +0700

    abstraction

--- test_project.py
+++ test_project.py
@@ -1,11 +1,11 @@
 import pytest
-import blackjack
+import project, blackjack
 
 def test_print_cards():
     testing_card_1 = blackjack.Card((chr(9829), 2), 2)
     testing_card_2 = blackjack.Card((chr(9830), 'Q'), 10)
-    assert blackjack.print_cards([testing_card_1, testing_card_2]) == ' ___   ___  \n|2  | |Q  | \n| ♥ | | ♦ | \n|__2| |__Q| \n'
-    assert blackjack.print_cards([testing_card_2, testing_card_1]) == ' ___   ___  \n|Q  | |2  | \n| ♦ | | ♥ | \n|__Q| |__2| \n'
+    assert project.print_cards([testing_card_1, testing_card_2]) == ' ___   ___  \n|2  | |Q  | \n| ♥ | | ♦ | \n|__2| |__Q| \n'
+    assert project.print_cards([testing_card_2, testing_card_1]) == ' ___   ___  \n|Q  | |2  | \n| ♦ | | ♥ | \n|__Q| |__2| \n'
     #TODO error checking
 
 def test_create_entity():
